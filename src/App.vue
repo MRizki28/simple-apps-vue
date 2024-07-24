f7c3ed0579611827bffb057dec22f28fce01a53e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-07-24 18:55:58 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="max-w-screen-xl mx-auto">
+    <div class="flex justify-center items-center">
+      <input
+        type="text"
+        id="search"
+        class="border p-3 mt-5 rounded-md w-96 border-black"
+        placeholder="Search..."
+        @input="handleSearch"
+      />
+    </div>
+    <div class="grid grid-cols-4 gap-3 gap-y-3">
+      <div
+        v-for="item in data"
+        :key="item.id"
+        class="max-w-sm rounded overflow-hidden shadow-lg"
+      > 
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{{ item.title }}</div>
+          <p class="text-gray-700 text-base">
+            {{ item.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import axios from "axios";
+import { defineComponent, onMounted, ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+interface Product {
+  id: number;
+  title: string;
+  description: string;
 }
+
+export default defineComponent({
+  name: "TestingFetchAPI",
+  setup() {
+    const data = ref<Product[]>([]);
+    const searchTerm = ref("");
+
+    const fetchApi = async () => {
+      try {
+        const response = await axios.get(
+          `https://dummyjson.com/products/search?q=${searchTerm.value}`
+        );
+        const responseData = response.data.products;
+        console.log(responseData);
+        data.value = responseData;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const handleSearch = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      console.log(target.value);
+      searchTerm.value = target.value;
+      fetchApi();
+    };
+
+    onMounted(() => {
+      fetchApi();
+    });
+
+    return {
+      data,
+      handleSearch,
+    };
+  },
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
